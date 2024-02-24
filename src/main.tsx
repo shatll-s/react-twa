2739ffa4def6f9a8cc7a8c3fca968eb7876860fa
shatll-s <shatll_s@mail.ru>
2024-02-24 17:35:46 +0300

    Almost done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://e4pool.com/assets/tonconnect-manifest.json'
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <App />
+    </TonConnectUIProvider>,
 )
